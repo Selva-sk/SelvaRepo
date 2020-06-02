3ab1efca6488552169846789aacd6e0f3b4e4112
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-02 23:12:31 +0530

    Calculating monthly wage of both Employee Types

--- add_part_emp.sh
+++ add_part_emp.sh
@@ -3,18 +3,22 @@
 full_time_hour=9
 part_time_hour=8
 wage_per_hour=20
-
+days=30
 type=$((RANDOM%2))
 
 case $type in
 	1) echo "Full Time Employee"
       wage=$(($full_time_hour * $wage_per_hour))
-      echo "Wage :" $wage
+      echo "Daily Wage :" $wage
+		monthly_wage=$(($wage * $days))
+      echo "Monthly wage :" $monthly_wage
 		;;
 	0) echo "Part Time Employee"
       wage=$(($part_time_hour * $wage_per_hour))
-      echo "Wage :" $wage
-		;;
+      echo "Daily Wage :" $wage
+		monthly_wage=$(($wage * $days))
+      echo "Monthly wage :" $monthly_wage
+      ;;
 	*) echo "**Invalid**"
 		;;
 esac
