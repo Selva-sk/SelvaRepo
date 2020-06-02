91183bcb5bae7ed66bfd629b251019097a84d5de
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-02 23:49:33 +0530

    Get Employee work hours using Functions

--- add_part_emp.sh
+++ add_part_emp.sh
@@ -8,6 +8,10 @@ total_days=0
 
 i=0
 
+function getHours(){
+echo "Total Hours Worked:" $total_hours
+}
+
 type=$((RANDOM%2))
 
 case $type in
@@ -20,17 +24,21 @@ case $type in
 		((total_days++))
 		((i++))
       done
+		total_hours=$(($total_hours-9))
+		getHours
       echo "Monthly wage :" $monthly_wage
 		;;
 	0) echo "Part Time Employee"
       while [[ $total_hours -lt 100 && $total_days -lt 20 ]]
       do
-      wage=$(($full_time_hour * $wage_per_hour))
+      wage=$(($part_time_hour * $wage_per_hour))
 		monthly_wage=$(($monthly_wage+$wage))
       total_hours=$((total_hours+8))
       ((total_days++))
       ((i++))
       done
+		total_hours=$(($total_hours-8))
+		getHours
       echo "Monthly Wage :" $monthly_wage
       ;;
 	*) echo "**Invalid**"
