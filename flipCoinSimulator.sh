56afbb103377d2c4d1d21590223338b99fd1d301
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-05 20:07:22 +0530

    v.1:Flip coin multiple times and show the winner/Tied game

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,15 +1,34 @@
 #!/bin/bash 
 
 echo "!!!!...Coin Simulator...!!!!"
+read -p "Enter no of times to flip:" no
 
 Heads=0
 Tails=0
-rand=$(( (RANDOM % 2 ) + 1 ))
+
+for((i=0; i<no; i++))
+do
+	rand=$(( (RANDOM % 2 ) + 1 ))
 if [ $rand -eq 1 ]
 then
-echo "!!...Heads...!!"
-((Heads++));
+	echo "!!...Heads...!!"
+	((Heads++));
 else
-echo "!!..Tails...!!"
-((Tails++));
+	echo "!!..Tails...!!"
+	((Tails++));
+fi
+done
+
+echo "Count :-  Heads : $Heads  Tails : $Tails"
+
+if [ $Heads -eq $Tails ]
+then
+	echo "Game Tied.."
+else
+if [ $Heads -gt $Tails ]
+then
+	echo "Heads won..."
+else 
+	echo "Tails won..."
+fi
 fi
