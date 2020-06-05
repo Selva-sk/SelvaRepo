fdd1858c6bf10f980fb0a92cc4288d37f5a0372c
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-05 23:40:16 +0530

    v.2:Flipping two coins together multiple times and calculating Doublet Percentage:-

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,33 +1,61 @@
 #!/bin/bash
 echo "!!..Flip Coin Combination..!!"
 read -p "Enter number of times to flip :" no
-declare -A Count;
+declare -A Coin1;
+declare -A Coin2;
+HH=0
+TT=0
+HT=0
+TH=0
 
-Heads=0
-Tails=0
-i=1
-
-for((i=1; i<=no; i++))
+for((i=1; $i<=no; i++))
 do
-rand=$(( (RANDOM % 2 ) + 1 ))
+	rand=$(( (RANDOM % 2 ) + 1 ))
 if [ $rand -eq 1 ]
 then
-((Heads++));
-Count[$i]="Head"
+	Coin1[$i]="H"
+	Array1[$i]="H"
 else
-((Tails++));
-Count[$i]="Tail"
+	Coin1[$i]="T"
+	Array1[$i]="T"
+fi
+	rand=$(( (RANDOM % 2 ) + 1 ))
+if [ $rand -eq 1 ]
+then
+	Coin2[$i]="H"
+	Array2[$i]="H"
+else
+	Coin2[$i]="T"
+	Array2[$i]="T"
+fi
+echo "${Coin1[$i]} ${Coin2[$i]}"
+
+if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'H' ]]
+then
+	((HH++))
+else
+if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'T' ]]
+then
+	((TT++))
+fi
+fi
+
+if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'T' ]]
+then
+	((HT++))
+else
+if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'H' ]]
+then
+	((TH++))
+fi
 fi
 done
-echo "Count : Heads : $Heads Tails : $Tails"
-echo "Dictionary elements:"
-for((i=1; i<=no; i++))
-do
-echo "Trial $i :" ${Count[$i]}
-done
 
-echo "Heads Singlet Percentage :"
-awk "BEGIN {print (($Heads/$no))}"
-echo "Tails Singlet Percentage :"
-awk "BEGIN {print (($Tails/$no))}"
-
+echo "HH Doublet Percentage :"
+awk "BEGIN {print (($HH/$no))}"
+echo "TT Doublet Percentage :"
+awk "BEGIN {print (($TT/$no))}"
+echo "HT Doublet Percentage :"
+awk "BEGIN {print (($HT/$no))}"
+echo "TH Doublet Percentage :"
+awk "BEGIN {print (($TH/$no))}"
