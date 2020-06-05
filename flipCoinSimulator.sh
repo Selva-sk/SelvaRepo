e7f010a65849e47d56ccea74ab9858216976c27e
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-05 21:01:30 +0530

    v.3:Continue the game after tie and stop once difference between Heads and Tails>=2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,7 +5,7 @@ echo "!!!!...Coin Simulator...!!!!"
 Heads=0
 Tails=0
 
-for((i=0; $Heads<21 && $Tails<21; i++))
+for((i=0; $Heads<21 || $Tails<21; i++))
 do
 	rand=$(( (RANDOM % 2 ) + 1 ))
 if [ $rand -eq 1 ]
@@ -22,7 +22,26 @@ echo "Count :-  Heads : $Heads  Tails : $Tails"
 
 if [ $Heads -eq $Tails ]
 then
-	echo "Game Tied.."
+	echo "Game Tied..Continuing again to reach minimum 2 differnce"
+for((j=0; $(($Heads-$Tails))<2 && $(($Tails-$Heads))<2; j++ ))
+do
+   rand=$(( (RANDOM % 2 ) + 1 ))
+if [ $rand -eq 1 ]
+then
+   echo "!!...Heads...!!"
+   ((Heads++));
+else
+   echo "!!..Tails...!!"
+   ((Tails++));
+fi
+done
+if [ $Heads -gt $Tails ]
+then
+   echo "Heads won..."
+else
+   echo "Tails won..."
+fi
+echo "Final Count :-  Heads : $Heads  Tails : $Tails"
 else
 if [ $Heads -gt $Tails ]
 then
