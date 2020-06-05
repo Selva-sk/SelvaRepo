3f2bcaa00b6600528431dfde6bfdfd3340d780b8
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-06 00:43:07 +0530

    v.1:Computation of expression (a+b*c)

--- ari_comp.sh
+++ ari_comp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 echo "Enter 3 values :"
-read -p "Enter value 1:" v1
-read -p "Enter value 2:" v2
-read -p "Enter value 3:" v3
+read -p "Enter value 1:" a
+read -p "Enter value 2:" b
+read -p "Enter value 3:" c
+
+exp1=$(($b*$c))
+exp1=$(($exp1+$a))
+echo $exp1
