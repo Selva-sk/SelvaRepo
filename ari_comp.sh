3ddda870b7b4f7040116d2dcb44f1826341b4b08
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-06 01:10:10 +0530

    v.5:Storing all Arithmetic expression values in a Dictionary

--- ari_comp.sh
+++ ari_comp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A values;
 echo "Enter 3 values :"
 read -p "Enter value 1:" a
 read -p "Enter value 2:" b
@@ -7,12 +8,15 @@ read -p "Enter value 3:" c
 exp1=$(($b*$c))
 exp1=$(($exp1+$a))
 echo $exp1
+values[1]=$exp1
 
 exp2=$(($a*$b))
 exp2=$(($exp2+$c))
 echo $exp2
+values[2]=$exp2
 
-awk "BEGIN {printf (($c+$a/$b))}"
+values[3]=`awk "BEGIN {printf (($c+$a/$b))}"`
 
-awk "BEGIN {printf (($a%$b+$c))}"
+values[4]=`awk "BEGIN {printf (($a%$b+$c))}"`
 
+echo "Values are :" ${values[@]}
