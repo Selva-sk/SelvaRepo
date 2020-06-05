a7942dcc42474e939134f295b9ef637c899347fe
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-06 00:02:42 +0530

    v.3:Flipping 3 coins together multiple times and calculating Triplet Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,10 +3,15 @@ echo "!!..Flip Coin Combination..!!"
 read -p "Enter number of times to flip :" no
 declare -A Coin1;
 declare -A Coin2;
-HH=0
-TT=0
-HT=0
-TH=0
+declare -A Coin3;
+HHH=0
+TTT=0
+HHT=0
+HTH=0
+THH=0
+TTH=0
+THT=0
+HTT=0
 
 for((i=1; $i<=no; i++))
 do
@@ -28,34 +33,67 @@ else
 	Coin2[$i]="T"
 	Array2[$i]="T"
 fi
-echo "${Coin1[$i]} ${Coin2[$i]}"
-
-if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'H' ]]
+   rand=$(( (RANDOM % 2 ) + 1 ))
+if [ $rand -eq 1 ]
 then
-	((HH++))
+   Coin3[$i]="H"
+   Array3[$i]="H"
 else
-if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'T' ]]
-then
-	((TT++))
-fi
+   Coin3[$i]="T"
+   Array3[$i]="T"
 fi
 
-if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'T' ]]
+echo "${Coin1[$i]} ${Coin2[$i]} ${Coin3[$i]}"
+
+if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'H' && ${Array3[$i]} == 'H' ]]
 then
-	((HT++))
+	((HHH++))
 else
-if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'H' ]]
+if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'T' && ${Array3[$i]} == 'T' ]]
 then
-	((TH++))
+	((TTT++))
 fi
+fi
+
+if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'H' && ${Array3[$i]} == 'T' ]]
+then
+	((HHT++))
+fi
+if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'T' && ${Array3[$i]} == 'H' ]]
+then
+   ((HTH++))
+fi
+if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'H' && ${Array3[$i]} == 'H' ]]
+then
+   ((THH++))
+fi
+if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'T' && ${Array3[$i]} == 'H' ]]
+then
+   ((TTH++))
+fi
+if [[ ${Array1[$i]} == 'T' && ${Array2[$i]} == 'H' && ${Array3[$i]} == 'T' ]]
+then
+   ((THT++))
+fi
+if [[ ${Array1[$i]} == 'H' && ${Array2[$i]} == 'T' && ${Array3[$i]} == 'T' ]]
+then
+   ((HTT++))
 fi
 done
 
-echo "HH Doublet Percentage :"
-awk "BEGIN {print (($HH/$no))}"
-echo "TT Doublet Percentage :"
-awk "BEGIN {print (($TT/$no))}"
-echo "HT Doublet Percentage :"
-awk "BEGIN {print (($HT/$no))}"
-echo "TH Doublet Percentage :"
-awk "BEGIN {print (($TH/$no))}"
+echo "HHH Triplet Percentage :"
+awk "BEGIN {print (($HHH/$no))}"
+echo "TTT Triplet Percentage :"
+awk "BEGIN {print (($TTT/$no))}"
+echo "HHT Triplet Percentage :"
+awk "BEGIN {print (($HHT/$no))}"
+echo "HTH Triplet Percentage :"
+awk "BEGIN {print (($HTH/$no))}"
+echo "THH Triplet Percentage :"
+awk "BEGIN {print (($THH/$no))}"
+echo "TTH Triplet Percentage :"
+awk "BEGIN {print (($TTH/$no))}"
+echo "THT Triplet Percentage :"
+awk "BEGIN {print (($THT/$no))}"
+echo "HTT Triplet Percentage :"
+awk "BEGIN {print (($HTT/$no))}"
