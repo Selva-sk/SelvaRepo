317d911b37f3d97784b5c19e8fc4aa122172d64c
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-06 07:29:55 +0530

    v.0:Name validation in User Registration

---
+++ name_valid.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+echo "Name validation...."
+read -p "Enter Firstname :" first
+
+pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $first =~ $pattern ]]
+then
+ echo "Valid Firstname.."
+else
+ echo "!!**Invalid**!!-Firstname should starts with CAPS"
+fi
+
+read -p "Enter Lastname :" last
+
+if [[ $last =~ $pattern ]]; then echo "Valid Lastname"; else echo "!!**Invalid**!!-Lastname should starts with CAPS"; fi
+
+echo "..END.."
