064d5675b685b3c6272dd02ee4e31253308f7b17
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-11 18:51:34 +0530

    Day19-UC2(JAVA):Calculating Daily wage of an Full Time Employee:-

--- Day16/wageCompJava.java
+++ Day16/wageCompJava.java
@@ -1,6 +1,9 @@
 import java.util.Random;
 
 public class wageCompJava{
+static int wagePerHour=20;
+static int fullDayHour=8;
+static int dailyWageFull=0;
 	public static void main(String[] args)
 	{
 		Random rand = new Random();
@@ -9,9 +12,15 @@ public class wageCompJava{
 		System.out.println("Checking Employee Attendance...");
 
 	if(random>50)
+		{
 		System.out.println("Employee is Present");
+		dailyWageFull=wagePerHour*fullDayHour;
+		System.out.println("Full Time Employee wage(Per Day):" + dailyWageFull);
+		}
 	else
+		{
 		System.out.println("Employee is Absent");
-
-}
+		System.out.println("Full Time Employee wage(Per Day):" + dailyWageFull);
+		}
+	}
 }
