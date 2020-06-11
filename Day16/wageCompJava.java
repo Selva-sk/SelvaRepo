e550d1b6752575de845367f85a6a2aa1d1b41891
“Selvakumar-sk” <selva.sk0505@gmail.com>
2020-06-11 19:59:32 +0530

    Day19-UC5(JAVA):Calculating Monthly wage of Part/Full Time Employee:-

--- Day16/wageCompJava.java
+++ Day16/wageCompJava.java
@@ -6,6 +6,10 @@ static int wageHourFull=20;
 static int dayHour=8;
 static int dailyWageFull=0;
 static int dailyWagePart=0;
+static int daysPerMonth=20;
+static int monthlyWageFull=0;
+static int monthlyWagePart=0;
+static int present,absent=0;
 
 public static void main(String[] args)
 	{
@@ -16,33 +20,44 @@ public static void main(String[] args)
 switch(random)
 {
 case 1:
- random = rand.nextInt(100);
-   if(random>=70)
+		dailyWageFull=wageHourFull*dayHour;
+		System.out.println("Full Time Employee wage(Per Day):" + dailyWageFull);
+	for(int i=1;i<=20;i++){ 
+		random = rand.nextInt(100);
+   if(random>=50)
       {
-      System.out.println("Full Time Employee is Present");
-      dailyWageFull=wageHourFull*dayHour;
-      System.out.println("Employee wage(Per Day):" + dailyWageFull);
-      }
+      System.out.println("Day"+ i +": Present");
+		monthlyWageFull+=dailyWageFull;
+		present++;
+		}
    else
       {
-      System.out.println("Full Time Employee is Absent");
-      System.out.println("Employee wage(Per Day):" + dailyWageFull);
+      System.out.println("Day"+ i +": Absent");
       }
+		}
+		System.out.println("No of Days worked:" + present);
+      System.out.println("Employee wage(Per Month):" + monthlyWageFull);
+
 break;
 
 case 0:
- random = rand.nextInt(100);
-   if(random<70)
+ 		dailyWagePart=wageHourPart*dayHour;
+      System.out.println("Part Time Employee wage(Per Day):" + dailyWagePart);
+   for(int i=1;i<=20;i++){
+      random = rand.nextInt(100);
+   if(random>=50)
       {
-      System.out.println("Part Time Employee is Present");
-      dailyWagePart=wageHourPart*dayHour;
-      System.out.println("Employee wage(Per Day):" + dailyWagePart);
+      System.out.println("Day"+ i +": Present");
+      monthlyWagePart+=dailyWagePart;
+      present++;
       }
    else
       {
-      System.out.println("Part Time Employee is Absent");
-      System.out.println("Employee wage(Per Day):" + dailyWagePart);
+      System.out.println("Day"+ i +": Absent");
       }
+      }
+      System.out.println("No of Days worked:" + present);
+      System.out.println("Employee wage(Per Month):" + monthlyWagePart);
 break;
 
 default:
